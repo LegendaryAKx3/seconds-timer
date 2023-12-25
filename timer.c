fd465fbf8baf133f260155d9614e78712daf03a5
LegendaryAKx3 <109812654+LegendaryAKx3@users.noreply.github.com>
2023-12-25 19:54:58 +0000

    Timer and output functions

--- timer.c
+++ timer.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 #include <argp.h>
+
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+int timer(int duration);
+int print(int seconds);
 
 const char *argp_program_version = "timer 1.0";
 
@@ -51,11 +61,37 @@ int main(int argc, char *argv[]) {
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
     if (arguments.seconds > 0) {
-        // TODO: timer
+        timer(arguments.seconds);
     } else {
         fprintf(stderr, "Error: Invalid value for SECONDS\n");
         return EXIT_FAILURE;
     }
 
     return EXIT_SUCCESS;
+}
+
+int print(int seconds) {
+    int minute = floor(seconds / 60);
+    int second = seconds - (minute * 60);
+    int hour = floor(minute / 60);
+    minute = minute - (hour * 60);
+    system("clear");
+    // printf("1. Start  2. Stop  3. Reset  4. End\n");
+    printf("===================================\n");
+    printf("               %d:%d:%d\n", hour, minute, second); 
+    printf("===================================\n");
+    return (seconds--);
+}
+
+int timer(int duration){
+  while (duration > 0) {
+    #ifdef _WIN32
+        Sleep( 1000 );
+    #else
+        sleep( 1 );
+    #endif
+    duration = print(duration);
+    
+  }
+  return 0;
 }
